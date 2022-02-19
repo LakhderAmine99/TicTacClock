95804c44bc6d6b81681813ef8812bd65cf96e628
Lakhder Amine <87427379+LakhderAmine99@users.noreply.github.com>
2022-02-19 19:04:26 +0100

    Update TicTacClock.js

--- src/TicTacClock.js
+++ src/TicTacClock.js
@@ -3,19 +3,25 @@ function TicTacClock(){
     let utils = new Utils();
     let canvasElm = utils.element('canvas');
     let angle = (Math.PI*2)/60;
+    let minutes = null;
+    let hours = null;
 
     let sprites = [];
     let images = [];
-    let sources = ["./public/assets/images/c1.png", "./public/assets/images/a1.png", "./public/assets/images/a2.png"];
+    let sources = 
+    [
+        ["./public/assets/images/c1.png",1106,1116],
+        ["./public/assets/images/a1.png",459,72],
+        ["./public/assets/images/a2.png",366,89]
+    ];
 
     for (let i = 0; i < 3; i++) 
     {
         images[i] = new Image();
-        images[i].src = sources[i];
-    }
+        images[i].src = sources[i][0];
+        images[i].width = sources[i][1];
+        images[i].height = sources[i][2];
 
-    for (let i = 0; i < 3; i++) 
-    {
         sprites[i] = Object.create(Sprite);
         sprites[i].image = images[i];
         sprites[i].x = 0;
@@ -26,12 +32,15 @@ function TicTacClock(){
 
     let clock = new Clock();
     let canvas = new Canvas(canvasElm, 400, 400);
+    
+    this.start = function(){
 
-    this.start = function (){
+        hours = clock.getRadHours();
+        minutes = clock.getRadMinutes();
 
-        loadImages(images);
+        loadImages();
 
-        window.setTimeout(function(){
+        setTimeout(function(){
             
             loadComplated();
             
@@ -40,40 +49,19 @@ function TicTacClock(){
         update();
     }
 
-    function loadImages(images){
+    function loadImages(){
 
         for (let i = 0; i < sprites.length; i++) 
         {
             let sprite = sprites[i];
 
-            sprite.image.onload = () => { console.log('images loaded') };
+            sprite.image.onload = () => { console.log('images loaded'); };
 
-            images[i].onerror = function () {
-
-                alert('error loading assets !');
-
-            };
+            sprite.image.onerror = () => { alert('error loading assets !'); };
         }
     };
 
     function loadComplated(){
-
-        render();
-    }
-
-    function update(){
-
-        window.setInterval(()=>{
-
-            render();
-            angle += (Math.PI*2)/60;
-
-        },1000);
-    }
-
-    function render() {
-
-        canvas.getContext().clearRect(0,0,canvas.canvas.width,canvas.canvas.height);
 
         sprites[0].x = canvas.centerX / 2 + 8;
         sprites[0].y = canvas.centerY / 2;
@@ -83,12 +71,28 @@ function TicTacClock(){
 
         sprites[2].x = sprites[0].centerx() - 5;
         sprites[2].y = sprites[0].centery() - 9;
+        
+        render();
+    }
 
-        angle = angle || 0;
+    function update(){
+
+        setInterval(()=>{
+
+            render();
+            minutes += (Math.PI*2)/60;
+
+        },1000);
+    }
+
+    function render() {
+
+        canvas.getContext().clearRect(0,0,canvas.canvas.width,canvas.canvas.height);
+        let sprite = null;
 
         for (let i = 0; i < sprites.length; i++) 
         {
-            let sprite = sprites[i];
+            sprite = sprites[i];
 
             if (i == 1 || i == 2) 
             {
@@ -97,11 +101,11 @@ function TicTacClock(){
 
                 if (i == 1) 
                 {
-                    canvas.getContext().rotate(angle - Math.PI/2);
+                    canvas.getContext().rotate(minutes);
                 } 
                 else 
                 {
-                    canvas.getContext().rotate(angle/60 - Math.PI/2);
+                    canvas.getContext().rotate(hours);
                 }
 
                 canvas.getContext().translate(-sprites[0].centerx(),-sprites[0].centery());
